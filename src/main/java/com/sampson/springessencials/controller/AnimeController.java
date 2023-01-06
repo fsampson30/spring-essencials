b163da5f219f79d36d994d4d2ba6e1d7f78dacf8
fsampson30 <fsampson30@gmail.com>
2023-01-06 09:34:06 -0300

    Created endpoint to findById.

--- src/main/java/com/sampson/springessencials/controller/AnimeController.java
+++ src/main/java/com/sampson/springessencials/controller/AnimeController.java
@@ -5,7 +5,10 @@ import com.sampson.springessencials.service.AnimeService;
 import com.sampson.springessencials.util.DateUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +27,14 @@ public class AnimeController {
     private AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }

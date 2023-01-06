d7742360bec3fc29c2440066f3e80ffa88aaa161
fsampson30 <fsampson30@gmail.com>
2023-01-06 10:27:00 -0300

    Created endpoint to save an Anime.

--- src/main/java/com/sampson/springessencials/controller/AnimeController.java
+++ src/main/java/com/sampson/springessencials/controller/AnimeController.java
@@ -7,10 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -36,5 +33,10 @@ public class AnimeController {
     public ResponseEntity<Anime> findById(@PathVariable long id){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/sampson/springessencials/service/AnimeService.java
+++ src/main/java/com/sampson/springessencials/service/AnimeService.java
@@ -5,15 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L,"DB2"), new Anime(2L,"Berserk"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"DB2"), new Anime(2L,"Berserk")));
+    }
 
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"DB2"), new Anime(2L,"Berserk"));
+        return animes;
     }
 
     public Anime findById(long id){
@@ -21,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(3,100000));
+        animes.add(anime);
+        return anime;
     }
 }

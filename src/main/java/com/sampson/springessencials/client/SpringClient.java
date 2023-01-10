24272e9bb5af5a505de5338faf5d960f0170c070
fsampson30 <fsampson30@gmail.com>
2023-01-10 10:47:33 -0300

    Created class to use RestTemplate.

---
+++ src/main/java/com/sampson/springessencials/client/SpringClient.java
@@ -0,0 +1,17 @@
+package com.sampson.springessencials.client;
+
+import com.sampson.springessencials.model.Anime;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+public class SpringClient {
+    public static void main(String[] args) {
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class,2);
+        log.info(entity);
+
+        Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
+        log.info(object.getId()+ " - " + object.getName());
+    }
+}

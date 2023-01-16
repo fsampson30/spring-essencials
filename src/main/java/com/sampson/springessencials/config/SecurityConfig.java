168f1768b679c703a278fd65ff082d5c0b6f4177
fsampson30 <fsampson30@gmail.com>
2023-01-16 14:07:43 -0300

    Added Antmatchers to configuration.

--- src/main/java/com/sampson/springessencials/config/SecurityConfig.java
+++ src/main/java/com/sampson/springessencials/config/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
 // Disabled for testing -> csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .authorizeRequests()
+                .antMatchers("/animes/admin/**").hasRole("ADMIN")
+                .antMatchers("/animes/**").hasRole("USER")
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/sampson/springessencials/controller/AnimeController.java
+++ src/main/java/com/sampson/springessencials/controller/AnimeController.java
@@ -50,7 +50,6 @@ public class AnimeController {
     }
 
     @GetMapping("by-id/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
                                                                  @AuthenticationPrincipal UserDetails userDetails){
         log.info(userDetails);
@@ -65,12 +64,11 @@ public class AnimeController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody){
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/admin/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id){
         animeService.delete(id);
         return ResponseEntity.noContent().build();

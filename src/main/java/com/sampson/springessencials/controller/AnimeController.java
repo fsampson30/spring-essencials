d2d81378751b3cd4de6cc969955bd490e5a23df2
fsampson30 <fsampson30@gmail.com>
2023-01-17 13:34:56 -0300

    Added more information to the documentation.

--- src/main/java/com/sampson/springessencials/controller/AnimeController.java
+++ src/main/java/com/sampson/springessencials/controller/AnimeController.java
@@ -5,6 +5,9 @@ import com.sampson.springessencials.requests.AnimePostRequestBody;
 import com.sampson.springessencials.requests.AnimePutRequestBody;
 import com.sampson.springessencials.service.AnimeService;
 import com.sampson.springessencials.util.DateUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.log4j.Log4j2;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,50 +36,57 @@ public class AnimeController {
     private AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable){
+    @Operation(summary = "List all animes paginated.",
+            description = "The default page is 20, use the parameter size to change the default value",
+            tags = {"anime"})
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Anime>> list(){
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.listAllNonPageable());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Anime> findById(@PathVariable long id){
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
 
     @GetMapping("by-id/{id}")
     public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
-                                                                 @AuthenticationPrincipal UserDetails userDetails){
+                                                                 @AuthenticationPrincipal UserDetails userDetails) {
         log.info(userDetails);
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
 
     @GetMapping("/find")
-    public ResponseEntity<List<Anime>> findByName(@RequestParam String name){
+    public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findByName(name));
     }
 
     @PostMapping
-    public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody){
+    public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/{id}")
-    public ResponseEntity<Void> delete(@PathVariable long id){
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "When Anime does not exists in the database")
+    } )
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping
-    public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody){
+    public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
         animeService.replace(animePutRequestBody);
         return ResponseEntity.noContent().build();
 
--- src/main/java/com/sampson/springessencials/requests/AnimePostRequestBody.java
+++ src/main/java/com/sampson/springessencials/requests/AnimePostRequestBody.java
@@ -1,5 +1,6 @@
 package com.sampson.springessencials.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.Objects;
 public class AnimePostRequestBody {
     @NotNull(message = "The anime name cannot be empty")
     @NotEmpty(message = "The anime name cannot be null")
+    @Schema(description = "This is the Anime's name", example = "Anime Test")
     private String name;
 
     public String getName() {
